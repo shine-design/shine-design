0edcff3d71eff4debf7898a01393ee54a8e1fbea
Longjun.Qu <154256698@qq.com>
2018-08-30 20:51:29 +0800

    libs path config

---
+++ libs/components/button/button.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class extends Component {
+    render() {
+        return (
+            <div>
+                THIS IS A TEST
+            </div>
+        );
+    }
+}
---
+++ libs/index.js
@@ -0,0 +1,3 @@
+
+//按钮组件a
+export {default as Button} from 'libs/components/button/button';
