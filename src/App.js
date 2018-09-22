d595cb2ba88a2b704c1044840cab44596342c926
Longjun.Qu <154256698@qq.com>
2018-09-22 08:44:04 +0800

    change demo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment} from 'react';
-import {Row, Col, Form, Input} from '../libs';
+import {Button} from '../libs';
 
 // import '../dist/index.css';
 
@@ -7,19 +7,7 @@ class App extends Component {
     render() {
         return (
             <Fragment>
-                <Row>
-                    <Col part={12}>
-                        <Form>
-                            <Input {...{
-                                label:'Email address',
-                                type:'email',
-                                placeholder:'Email address',
-                                helper:'We\'ll never share your email with anyone else.'
-                            }}/>
-                        </Form>
-                    </Col>
-                </Row>
-
+                <Button value='Button' />
             </Fragment>
         );
     }
