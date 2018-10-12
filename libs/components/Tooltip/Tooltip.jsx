d970a2504510464f6ed36b4fc7f566fcfba7abae
Longjun.Qu <154256698@qq.com>
2018-10-12 10:12:40 +0800

    tooltip component

--- libs/components/Tooltip/Tooltip.jsx
+++ libs/components/Tooltip/Tooltip.jsx
@@ -7,15 +7,16 @@
 
 // 核心依赖
 import React, {Component, Fragment} from 'react';
-import {findDomNode} from 'react-dom'
+import ReactDom from 'react-dom';
 import * as PropTypes from 'prop-types';
 
 // 第三方依赖库
 import classNames from 'classnames'
 import _ from 'lodash';
+import jQuery from 'jquery';
+import 'bootstrap/js/src/tooltip';
 
 // 组件依赖
-import 'bootstrap/js/src/tooltip';
 import Button from '../Button/Button';
 
 // 样式
@@ -24,19 +25,53 @@ import './style';
 export default class extends Component {
     constructor(props) {
         super(props);
+        this.child = React.createRef();
+        this.initTooltip = this.initTooltip.bind(this);
+        this.findChildDom = this.findChildDom.bind(this);
     }
 
     componentDidMount() {
-        // const {children} = this.props;
-        console.log(this.props.children);
-        console.log(findDomNode);
+        const {isOpen, onBeforeOpen, onOpened, onBeforeClose, onClosed} = this.props;
+        if (this.findChildDom()) {
+            const _child = this.findChildDom();
+
+            jQuery(_child).on('show.bs.tooltip', (e) => {
+                _.isFunction(onBeforeOpen) && onBeforeOpen(e, _child);
+            });
+            jQuery(_child).on('shown.bs.tooltip', (e) => {
+                _.isFunction(onOpened) && onOpened(e, _child);
+            });
+            jQuery(_child).on('hide.bs.tooltip', (e) => {
+                _.isFunction(onBeforeClose) && onBeforeClose(e, _child);
+            });
+            jQuery(_child).on('hidden.bs.tooltip', (e) => {
+                _.isFunction(onClosed) && onClosed(e);
+            });
+
+            this.initTooltip(_child);
+        }
+    }
+
+    componentWillUnmount() {
+        this.findChildDom() && jQuery(this.findChildDom()).tooltip('destroy');
     }
 
     static propTypes = {
+        isOpen: PropTypes.bool,
+        title: PropTypes.string,
+        delay: PropTypes.oneOfType([
+            PropTypes.number,
+            PropTypes.shape({
+                show: PropTypes.number,
+                hide: PropTypes.number
+            })
+        ]),
         isShow: PropTypes.bool,
         isHtml: PropTypes.bool,
         isAutoWidth: PropTypes.bool,
+        isAnimate: PropTypes.bool,
         skin: PropTypes.oneOf(['light', 'dark']),
+        placement: PropTypes.oneOf(['top', 'left', 'right', 'bottom']),
         children: (props, propName, componentName) => {
             if (props[propName].type !== Button) {
                 return new Error(
@@ -48,15 +83,31 @@ export default class extends Component {
     };
 
     static defaultProps = {
-        trigger: 'hover',
-        skin: 'dark',
+        isOpen: true,
+        title: '',
+        trigger: ['hover', 'focus'],
+        skin: 'light',
+        delay: 0,
         isShow: false,
         isHtml: false,
-        isAutoWidth: true
+        isAutoWidth: true,
+        isAnimate: true
     };
 
+    findChildDom() {
+        const {children} = this.props;
+
+        if (!_.isUndefined(children) && children.type === Button) {
+            const child = this.child.current;
+            if (child) {
+                return ReactDom.findDOMNode(child);
+            }
+        }
+        return null;
+    }
+
     initTooltip(el) {
-        const {trigger, isAutoWidth} = this.props;
+        const {skin, delay, isAutoWidth} = this.props;
 
         const _className = classNames(
             'sh-tooltip',
@@ -67,8 +118,8 @@ export default class extends Component {
             }
         );
 
-        el.tooltip({
-            trigger,
+        jQuery(el).tooltip({
+            delay,
             template: `<div class="${_className}" role="tooltip">\
                 <div class="arrow"></div>\
                 <div class="tooltip-inner"></div>\
@@ -77,22 +128,23 @@ export default class extends Component {
     }
 
     render() {
-        const {trigger, skin, offset, content, isHtml, children} = this.props;
+        const {trigger, skin, offset, title, placement, isAnimate, isHtml, children} = this.props;
 
         const {attributes} = children.props;
 
         _.merge(attributes, {
-            "data-toggle": "m-tooltip",
-            "data-trigger": trigger,
+            "data-trigger": _.isArray(trigger) ? trigger.join(' ') : trigger,
             "data-skin": skin,
             "data-offset": offset,
-            "data-original-title": content,
-            "data-html": isHtml
+            "data-original-title": title,
+            "data-html": isHtml,
+            "data-placement": placement,
+            'data-animation': isAnimate
         });
 
         return (
             <Fragment>
-                {!_.isUndefined(children) && children.type === Button && children}
+                {!_.isUndefined(children) && children.type === Button && React.cloneElement(children, {ref: this.child})}
             </Fragment>
         );
     }
