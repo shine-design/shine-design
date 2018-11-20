cc9dfae7a532b10f08d4583654cecc73966afa38
瞿龙俊 <154256698@qq.com>
2018-11-20 09:05:07 +0800

    表单校验规则

--- libs/components/Form/Form.jsx
+++ libs/components/Form/Form.jsx
@@ -7,6 +7,7 @@
 
 // 核心依赖
 import React, {Component, Fragment} from 'react';
+import ReactDom from "react-dom";
 import * as PropTypes from 'prop-types';
 import {
     COMMON_PROPS_TYPE,
@@ -16,38 +17,118 @@ import {
 // 第三方依赖库
 import classNames from 'classnames'
 import _ from 'lodash';
+import jQuery from 'jquery';
+import 'jquery-validation';
+import 'jquery-validation/dist/localization/messages_zh';
+import 'jquery-validation/dist/additional-methods';
+import '../../plugins/validate_init';
 
 // 组件依赖
-
+import Alert from '../Alert/Alert';
 // 样式
 import './style';
 
 export default class extends Component {
     constructor(props) {
         super(props);
+        this.formRef = React.createRef();
+        this.alertRef = React.createRef();
+    }
+
+    componentDidMount() {
+        const {validate, onSubmit, children} = this.props;
+        const {
+            isDebug = false,
+            rules = {},
+            messages = {},
+            ignore = '',
+            onErrorPlacement,
+            onSuccess,
+            onErrors, // 需要手动渲染错误提示，验证不通过时使用
+            onInvalidHandler // 无需手动渲染错误提示，验证不通过时使用
+        } = validate;
+        const formRef = this.formRef.current;
+
+        if (formRef) {
+            const _dom = ReactDom.findDOMNode(formRef);
+            const _alert = ReactDom.findDOMNode(this.alertRef.current);
+            const _rules = [];
+            const _messages = [];
+
+            _.forEach(_.isArray(children) ? children : [children], _child => {
+                const {name, validate} = _child.props;
+                if ((!_.isUndefined(name) && !_.isUndefined(validate))) {
+                    const _rule = [];
+                    const _message = [];
+                    _.forOwn(validate, function (result, key) {
+                        if (_.isObject(result)) {
+                            !_.isUndefined(result.value) && _rule.push({[key]: result.value});
+                            !_.isUndefined(result.message) && _message.push({[key]: result.message});
+                        } else {
+                            _rule.push({[key]: result});
+                        }
+                    });
+                    _rule.length !== 0 && _rules.push({[name]: _.assignIn({}, ..._rule)});
+                    _message.length !== 0 && _messages.push({[name]: _.assignIn({}, ..._message)});
+                }
+            });
+
+            const _ruleConfig = _.assignIn({}, ..._rules);
+            const _messageConfig = _.assignIn({}, ..._messages);
+
+            jQuery(_dom).validate({
+                rules: _.assignIn({}, _ruleConfig, rules),
+                messages: _.assignIn({}, _messageConfig, messages),
+                errorPlacement: onErrorPlacement,
+                success: onSuccess,
+                showErrors: onErrors,
+                debug: isDebug,
+                submitHandler: onSubmit,
+                invalidHandler: _.isFunction(onInvalidHandler) ? onInvalidHandler : (() => {
+                    jQuery(_alert).removeClass('sh--hide').show();
+                })
+            });
+
+            console.log(jQuery(_dom).valid());
+        }
     }
 
     static propTypes = {
-        ...COMMON_PROPS_TYPE
+        ...COMMON_PROPS_TYPE,
+        validate: PropTypes.object
     };
 
     static defaultProps = {
-        ...COMMON_PROPS_DEFAULT
+        ...COMMON_PROPS_DEFAULT,
+        validate: {}
     };
 
     render() {
         const {className, attributes, children} = this.props;
         return (
             <Fragment>
-                <form className={
+                <form ref={this.formRef} className={
                     classNames(
                         'sh-form',
+                        'sh-form--state',
+                        'sh-form--fit',
                         ...(
                             _.isArray(className) ? className : [className]
                         )
                     )}
                       {...attributes}
                 >
+                    <div ref={this.alertRef} className={
+                        classNames('sh-form__content', 'sh--hide')
+                    }>
+                        <Alert {...{
+                            color: 'danger',
+                            title: 'Oh snap! ',
+                            isAir: true,
+                            message: 'Change a few things up and try submitting again.',
+                            iconName: 'sh-icon-warning'
+                        }}/>
+                    </div>
                     {children}
                 </form>
             </Fragment>
--- src/App.js
+++ src/App.js
@@ -9,21 +9,74 @@ class App extends Component {
                 <Row>
                     <Col part={6} offset={3}>
                         <p>欢迎使用 Shine Design ！</p>
-                        <Form>
-                            <DatePicker {...{
-                                orientation: 'bottom left',
-                                todayHighlight: true,
-                                todayBtn: true,
-                                clearBtn: true
-
-                            }}>
-                                <Input {...{
-                                    label: '普通文本',
-                                    type: 'text',
-                                    placeholder: '请输入内容',
-                                    defaultValue: '欢迎使用Shine'
-                                }}/>
-                            </DatePicker>
+                        <Form {...{
+                            validate: {
+                            }
+                        }}>
+                            <Input {...{
+                                label: '普通文本',
+                                type: 'text',
+                                placeholder: '请输入内容',
+                                name: 'testA',
+                                validate: {
+                                    required: true,
+                                    minlength: 5
+                                }
+                            }}/>
+                            <Input {...{
+                                label: '密码文本',
+                                type: 'password',
+                                placeholder: '请输入密码',
+                                name: 'testB'
+                            }}/>
+                            <Input {...{
+                                label: 'Email',
+                                type: 'email',
+                                placeholder: '请输入电子邮箱',
+                                defaultValue: 'aaa',
+                                name: 'testC',
+                                validate: {
+                                    email: true
+                                }
+                            }}/>
+                            <Input {...{
+                                label: '数字文本',
+                                type: 'number',
+                                placeholder: '请输入数字',
+                                defaultValue: '1',
+                                name: 'testD',
+                                validate: {
+                                    min: 10
+                                }
+                            }}/>
+                            <Input {...{
+                                label: '网址文本',
+                                type: 'url',
+                                placeholder: '请输入网址',
+                                name: 'testE',
+                                validate: {
+                                    required: {
+                                        value: true,
+                                        message: '测试失败消息'
+                                    },
+                                    url: true
+                                }
+                            }}/>
+                            <Input {...{
+                                label: '多行文本',
+                                type: 'textarea',
+                                placeholder: '请输入文本内容',
+                                attributes: {
+                                    rows: 3
+                                },
+                                name: 'testF',
+                                validate: {
+                                    minlength: {
+                                        value: 10,
+                                        message: 'aaa'
+                                    }
+                                }
+                            }}/>
                         </Form>
                     </Col>
                 </Row>
