19055004d56962b46ea4dfb87365983c5a072455
瞿龙俊 <154256698@qq.com>
2019-05-13 20:27:21 +0800

    button props

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -13,7 +13,7 @@ import {classPrefix} from 'variables';
 
 class Button extends PureComponent {
   render() {
-    const {type, className, attributes, children} = this.props;
+    const {type, bgColor, outlineColor, gradient, className, attributes, children} = this.props;
 
     /** 计算样式 */
     const classes = classNames(
@@ -37,6 +37,21 @@ class Button extends PureComponent {
 Button.propTypes = {
   /** 按钮类型，支持普通按钮:button，提交按钮:submit，重置按钮:reset */
   type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  /** 按钮背景颜色 */
+  bgColor: PropTypes.string,
+  /** 按钮轮廓颜色 */
+  outlineColor: PropTypes.string,
+  /** 按钮渐变色，支持设置两个属性：from：起始颜色，to：终止颜色 */
+  gradient: PropTypes.shape({
+    from: PropTypes.string,
+    to: PropTypes.string,
+  }),
+  /** 按钮类型尺寸，支持 default：默认尺寸 / small：小尺寸 / large：大尺寸 */
+  size: PropTypes.oneOf(['default', 'small', 'large']),
+  /** 按钮是否被激活 */
+  isActive: PropTypes.bool,
+  /** 按钮是否被禁用 */
+  isDisabled: PropTypes.bool,
   /** 用户自定义修饰符 */
   className: PropTypes.string,
   /** 用户自定义属性 */
@@ -45,6 +60,12 @@ Button.propTypes = {
 
 Button.defaultProps = {
   type: 'button',
+  bgColor: 'primary',
+  outlineColor: undefined,
+  gradient: undefined,
+  size: 'default',
+  isActive: false,
+  isDisabled: false,
   className: '',
   attributes: {},
 };
